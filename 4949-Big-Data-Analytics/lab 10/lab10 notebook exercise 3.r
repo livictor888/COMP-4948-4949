811c82a2e3b00c35ca7dbe04f74066e16f39e178
livictor888 <victorli@live.ca>
2023-03-26 18:23:24 -0700

    week 10 lab

---
+++ 4949-Big-Data-Analytics/lab 10/lab10 notebook exercise 3.r
@@ -0,0 +1,20 @@
+# Databricks notebook source
+# Print string.
+print("Hello World!")
+
+# Append words
+print(paste("How","are","you?"), quote = FALSE)
+
+
+# COMMAND ----------
+
+# Print string.
+print("Victor")
+
+# Append words
+print(paste("How","are","you?"), quote = FALSE)
+
+
+# COMMAND ----------
+
+
